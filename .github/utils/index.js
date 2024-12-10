64541f2fd6816daa602e650d2f389f3990c78807
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-10 18:10:10 -0500

    add: matrix environmnet

--- .github/utils/index.js
+++ .github/utils/index.js
@@ -1,1 +1,4 @@
-console.log('sandbox secrets: ', JSON.parse(process.env.SANDBOX_SECRETS));
+const envJson = JSON.parse(process.env.SANDBOX_SECRETS);
+const sandbox = process.env.SANDBOX
+
+console.log(envJson[sandbox]);
