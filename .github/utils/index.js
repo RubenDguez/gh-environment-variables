a6a4309c67803933e11673f2707ad23b5ad889e7
Ruben Dominguez <argenis.dominguez@hotmail.com>
2024-12-10 19:19:52 -0500

    update: yml file

--- .github/utils/index.js
+++ .github/utils/index.js
@@ -1,9 +1,9 @@
 const fs = require('fs');
 
 const pass = {
-    'neersit': 'sit',
-    'neerqa': 'qa',
-    'neeruat': 'uat'
+    neersit: 'sit',
+    neerqa: 'qa',
+    neeruat: 'uat'
 }
 
 fs.writeFileSync('pass.txt', JSON.stringify(pass, null, 2), { encoding: 'utf-8' });
